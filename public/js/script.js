b6e90853cb468ff6cc4d9dc48628e099e2ec8278
meziani <meziani75011@gmail.com>
2025-05-15 03:00:37 +0200

    .

--- public/js/script.js
+++ public/js/script.js
@@ -52,18 +52,45 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Détection mobile
-//     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-//     // Détection Chrome mobile
-//     var isChrome = /Chrome/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent);
+document.addEventListener("DOMContentLoaded", function() {
+    // Détection mobile
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    // Détection Chrome mobile
+    var isChrome = /Chrome/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent);
 
-//     if (isMobile && isChrome) {
-//         var iframe = document.getElementById('haWidget-mobile');
-//         var link = document.getElementById('helloasso-link');
-//         if (iframe && link) {
-//             iframe.style.display = 'none';
-//             link.style.display = 'block';
-//         }
-//     }
-// });
+    if (isMobile && isChrome) {
+        var iframe = document.getElementById('haWidget-mobile');
+        var link = document.getElementById('helloasso-link');
+        if (iframe && link) {
+            iframe.style.display = 'none';
+            link.style.display = 'block';
+        }
+    }
+    
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+      const container = document.getElementById("helloasso-container");
+      const ua = navigator.userAgent.toLowerCase();
+      const isChromeMobile = /chrome/.test(ua) && /mobile/.test(ua);
+
+      if (isChromeMobile) {
+        container.innerHTML = `
+          <a href="https://www.helloasso.com/associations/association-cultuelle-de-la-commune-d-ezanville/formulaires/2/widget"
+             target="_blank"
+             rel="noopener noreferrer"
+             class="btn-don">
+            Faire un don sur HelloAsso
+          </a>`;
+      } else {
+        container.innerHTML = `
+          <iframe
+  id="haWidget"
+  allowtransparency="true"
+  scrolling="no"
+  sandbox="allow-scripts allow-forms allow-same-origin allow-popups allow-modals allow-presentation"
+  src="https://www.helloasso.com/..."
+  style="width: 100%; height: 120VH; border: none;">
+</iframe>`;
+      }
+    });
